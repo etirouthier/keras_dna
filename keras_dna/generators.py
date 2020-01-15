6fa8ff711c832885e51f392d27196af82508952a
erouthier <etienne.routhier@upmc.fr>
2020-01-15 11:30:02 +0100

    renaming directory

---
+++ keras_dna/generators.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 14:21:30 2019
+
+@author: routhier
+"""
+
+import numpy as np
+import inspect
+from copy import deepcopy
+
+from .sequence import SeqIntervalDl, StringSeqIntervalDl
+from .normalization import Weights
+from .utils import ArgumentsDict
+
+class Generator(object):
+    """
+    info:
+        doc: >
+            Generator for keras model able to yield inputs and targets in batch
+            by reading into a fasta file and a annotation file. Inputs are one-
+            hot-encoded or string.
+     args:
+         batch_size:
+             number of example per batch pass to the model.
+         one-hot-encoding:
+             whether or not the inputs is one-hot-encoded (False: string)
+             default: True
+         output_shape:
+             How to modify the shape of the output (because the initial output
+             structure is (batch, length, nb_types, nb_annotation) or (batch,
+             nb_types, nb_annotation))
+         weighting_mode:
+            {None, 'balanced', tuple(weights, bins), tuple(weight_pos, weight_neg)}
+            the methodology to set the weights.
+            For continuous dataset a tuple with the weights to apply and the
+            bins can be parsed (len(weights) == len(bins) - 1, and the smallest
+            bin must be smaller than the minimum of the data.)
+            For sparse dataset a tuple with the weight to apply for positive
+            (in [0]) and negative class (in [1]). Positive class refers to
+            labels with at least one positive value.
+            default: None
+        bins:
+            number of bins to apply to a continuous dataset before calculating
+            the probability of classes. Can also be an array of bins or 'auto'
+            for on optimized shearch of bins.
+            default='auto'
+         args:
+             arguments specific to the different dataloader that can be used.
+         kwargs:
+             dictionnary with specific arguments to the dataloader.
+    """
+    
+    def __init__(self, batch_size,
+                       one_hot_encoding=True,
+                       output_shape=None,
+                       weighting_mode=None,
+                       bins='auto',
+                       *args,
+                       **kwargs):
+        self.one_hot_encoding = one_hot_encoding
+        self.output_shape = output_shape
+        self.weighting_mode = weighting_mode
+        self.bins = bins
+        self.frame = inspect.currentframe()
+        
+        if self.one_hot_encoding:
+            self.dataset = SeqIntervalDl(*args,
+                                         **kwargs)
+        else:
+            self.dataset = StringSeqIntervalDl(*args,
+                                               **kwargs)
+        self.batch_size = batch_size
+        
+        if self.weighting_mode:
+            self.weights = Weights(self.dataset,
+                                   self.weighting_mode,
+                                   self.bins)
+
+    def __call__(self):
+        """Returns a generator to train a keras model (yielding inputs and
+        outputs)."""
+        def generator_function(dataset, batch_size):
+            indexes = np.arange(0, len(dataset), 1)
+            number_of_batches = len(dataset) // batch_size
+            
+            while True:
+            # reshuffled the train set after an epoch
+                np.random.shuffle(indexes)
+                
+                for num in range(number_of_batches):
+                    batch_indexes = indexes[num*batch_size : (num + 1) * batch_size]
+                    data = dataset[list(batch_indexes)]
+                    inputs = data['inputs']
+                    outputs = data['targets']
+                    
+                    if self.output_shape:
+                        outputs = outputs.reshape((outputs.shape[0],) +\
+                                                  self.output_shape[1:])
+                    if self.weighting_mode:
+                        weights = self.weights.find_weights(outputs)
+                        
+                        yield inputs, outputs, weights
+                    else:
+                        yield inputs, outputs
+            
+        return generator_function(self.dataset, self.batch_size)
+    
+    def __len__(self):
+        return len(self.dataset) // self.batch_size
+    
+    @property
+    def command_dict(self):
+        return ArgumentsDict(self, called_args='dataset')
+
+
+class MultiGenerator(object):
+    """
+    info:
+        doc: >
+            Class able to yield inputs and targets from several different
+            interval readers. Usefull to train on several species or to train
+            on both direct and reverse side.
+     args:
+         batch_size:
+             number of example per batch pass to the model.
+         dataset_list:
+             list of SeqIntervalDl or StringSeqIntervalDl example, the output
+             shape need to be the same for all instance.
+        inst_per_dataset:
+            list of integer, number of example to be taken from each dataset.
+            default='all'
+        output_shape:
+             How to modify the shape of the output (because the initial output
+             structure is (batch, length, nb_types, nb_annotation) or (batch,
+             nb_types, nb_annotation))
+    """
+    def __init__(self, batch_size,
+                       dataset_list,
+                       inst_per_dataset='all',
+                       output_shape=None):
+        self.dataset_list = dataset_list
+        self.batch_size = batch_size
+        self.inst_per_dataset = inst_per_dataset
+        self.output_shape = output_shape
+        self.frame = inspect.currentframe()
+
+    def __call__(self):
+        """Returns a generator to train a keras model (yielding inputs and
+        outputs)."""
+        indexes = self._get_indexes()
+        def generator_function(list_of_dataset, batch_size):
+            number_of_batches = len(indexes) // batch_size
+            
+            while True:
+            # reshuffled the train set after an epoch
+                np.random.shuffle(indexes)
+            
+                for num in range(number_of_batches):
+                    batch_indexes = indexes[num*batch_size : (num + 1) * batch_size]
+                    inputs = None
+                    targets = None
+
+                    for dataset_index, dataset in enumerate(list_of_dataset): 
+                        sub_batch_indexes = batch_indexes[batch_indexes[:, 0] == dataset_index]
+                        data = list_of_dataset[dataset_index][sub_batch_indexes[:, 1].tolist()]
+                        inputs = self._append_data(inputs, data['inputs'])
+                        targets = self._append_data(targets, data['targets'])
+                        if self.output_shape:
+                            targets = targets.reshape((targets.shape[0],) +\
+                                                      self.output_shape[1:])
+                        yield inputs, targets
+            
+        return generator_function(self.dataset_list, self.batch_size)
+
+    def _append_data(self, ldata, rdata):
+        if isinstance(ldata, list):
+            if len(rdata[0]) != 0:
+                ldata[0] = np.append(ldata[0], rdata[0], axis=0)
+                ldata[1] = np.append(ldata[1], rdata[1], axis=0)
+        
+        elif isinstance(ldata, np.ndarray):
+            if len(rdata) != 0:
+                ldata = np.append(ldata, rdata, axis=0)
+        else:
+            if isinstance(rdata, list):
+                if len(rdata[0]) != 0:
+                    ldata = rdata
+            elif isinstance(rdata, np.ndarray):
+                if len(rdata) != 0:
+                    ldata = rdata
+        return ldata
+
+    def _get_indexes(self):
+        if not self.inst_per_dataset == 'all':
+            assert len(self.dataset_list) == len(self.inst_per_dataset),\
+            """To pass the number of examples to be taken from every dataset,
+            the list of dataset and list of number must be of the same length
+            """
+
+        indexes = np.zeros((1, 2))
+        if self.inst_per_dataset == 'all':
+            self.inst_per_dataset = [len(dataset) for dataset in self.dataset_list]
+        
+        for dataset_index, dataset in enumerate(self.dataset_list):
+            length = len(dataset)
+            nb_inst = self.inst_per_dataset[dataset_index]
+            indexes_ = np.append(np.repeat([[dataset_index]], nb_inst,
+                                           axis=0),
+                                 np.random.choice(np.arange(length),
+                                                  nb_inst,
+                                                  replace=False).reshape(nb_inst,
+                                                                         1),
+                                 axis=1)
+
+            indexes = np.append(indexes, indexes_, axis=0)
+        indexes = indexes[1:].astype(int)
+        return indexes
+
+    def __len__(self):
+        return len(self._get_indexes()) // self.batch_size
+
+    @property
+    def command_dict(self):
+        argsdict_list = [dataset.command_dict for dataset in self.dataset_list]
+        argsdict_list.append(ArgumentsDict(self, kwargs=False))
+        return argsdict_list
+
+
+class PredictionGenerator(object):
+    """
+    info:
+        doc: >
+            Takes the command dict of a Generator instance (or a SeqIntervalDl
+            or a StringSeqIntervalDl instance) and returns a generator needed
+            to predict all along chromosomes.
+    
+    args:
+        batch_size:
+            integer
+        chrom_size:
+            A .sizes file with the size of chromosomes in two columns.
+        command_dict:
+            An ArgumentsDict instance that described how the model was trained.
+        incl_chromosomes:
+            list of chromosome to predict on.
+        fasta_file:
+            A fasta_file to make prediction on (if None the same as in
+            command_dict)
+            default=None
+    """
+    def __init__(self,
+                 batch_size,
+                 command_dict,
+                 chrom_size,
+                 incl_chromosomes,
+                 fasta_file=None):
+        self.batch_size = batch_size
+        self.command_dict = command_dict
+        self.chrom_size = chrom_size
+
+        if isinstance(incl_chromosomes, list):
+            self.incl_chromosomes = incl_chromosomes
+        else:
+            self.incl_chromosomes = [incl_chromosomes]
+        self.detailed_dict = command_dict.get_details()
+
+        if 'sequence.SparseDataset' in self.detailed_dict:
+            dataset_dict = self.detailed_dict['sequence.SparseDataset']
+            self.window = dataset_dict['length']
+            self.tg_window = 1
+
+            if dataset_dict['seq2seq']:
+                self.tg_window = self.window
+
+        elif 'sequence.ContinuousDataset' in self.detailed_dict:
+            dataset_dict = self.detailed_dict['sequence.ContinuousDataset']
+            self.window = dataset_dict['window']
+
+            if dataset_dict['downsampling']:
+                self.tg_window = dataset_dict['window']
+                self.sampling_len = dataset_dict['window'] // dataset_dict['tg_window']
+            else:
+                self.tg_window = dataset_dict['tg_window']
+
+        string_dict = deepcopy(self.detailed_dict['sequence.StringSeqIntervalDl'])
+        string_dict['annotation_files'] = self.chrom_size
+        string_dict['use_strand'] = False
+        if fasta_file:
+            string_dict['fasta_file'] = fasta_file
+
+        continuous_dict = {'ignore_targets' : True,
+                           'overlapping' : False,
+                           'window' : self.window,
+                           'incl_chromosomes' : self.incl_chromosomes,
+                           'tg_window' : self.tg_window}
+
+        if 'sequence.SeqIntervalDl' in self.detailed_dict:
+            self.input_dict = deepcopy(self.detailed_dict['sequence.SeqIntervalDl'])
+            self.input_dict.update(string_dict)
+            self.input_dict.update(continuous_dict)
+            self.dataset = SeqIntervalDl(**self.input_dict)
+        else:
+            self.input_dict = string_dict
+            self.input_dict.update(continuous_dict)
+            self.dataset = StringSeqIntervalDl(**self.input_dict)
+
+    def __len__(self):
+        return len(self.dataset) // self.batch_size
+
+    def __call__(self):
+        """Returns a generator to train a keras model (yielding inputs and
+        outputs)."""
+        def generator_function(dataset, batch_size):
+            indexes = np.arange(len(dataset))
+            number_of_batches = len(dataset) // batch_size
+
+            while True:
+                for num in range(number_of_batches):
+                    batch_indexes = indexes[num*batch_size : (num + 1) * batch_size]
+                    data = dataset[list(batch_indexes)]
+                    yield data['inputs'], data['metadata']
+
+        return generator_function(self.dataset, self.batch_size)
+
+    @property
+    def index_df(self):
+        """
+        Returns a DataFrame with the first and last index for every chromosome
+        and the position of the begining (included) of the prediction and the
+        end (excluded) of the prediction on the chromosome.
+        """
+        if 'sequence.SeqIntervalDl' in self.detailed_dict:
+            df = deepcopy(self.dataset.seq_dl.dataset.df)
+        else:
+            df = deepcopy(self.dataset.dataset.df)
+
+        if 'sequence.SparseDataset' in self.detailed_dict:
+            dataset_dict = self.detailed_dict['sequence.SparseDataset']
+
+            if dataset_dict['seq2seq']:
+                df['start'] = df.start.values - self.window // 2
+                df['stop'] = df.start.values + (df.last_index.values - df.first_index.values)\
+                            * self.window + self.window
+            else:
+                df['start'] = df.start.values
+                df['stop'] = df.start.values + (df.last_index.values - df.first_index.values)\
+                            + 1
+        elif 'sequence.ContinuousDataset' in self.detailed_dict:
+            dataset_dict = self.detailed_dict['sequence.ContinuousDataset']
+
+            if dataset_dict['downsampling']:
+                df['start'] = df.start.values - self.window // 2
+                df['stop'] = df.start.values + (df.last_index.values - df.first_index.values)\
+                            * self.window + self.window
+            else:
+                tg_window = dataset_dict['tg_window']
+                df['start'] = df.start.values - tg_window // 2
+                df['stop'] = df.start.values + (df.last_index.values - df.first_index.values)\
+                            * tg_window + tg_window
+        return df
---
+++ keras_dna/normalization.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 16:40:31 2019
+
+@author: routhier
+"""
+
+import pyBigWig
+import re
+import numpy as np
+
+
+class Normalizer(object):
+    """
+    Normalize the data by standard procedure. It takes the wanted procedure and
+    a bbi file as input. When called with a sequence extracted from the bbi
+    file, the sequence is normalized.
+        
+    args:
+        normalization:
+            {zscore, max, perctrim, min_max, None}, name of the procedure to be
+            applied.
+        bbi_file:
+            The file on which the data will be taken and then normalized.
+        threshold:
+            The pourcentage above which the distribution will be trimed.
+    """ 
+    def __init__(self,
+                 normalization,
+                 bbi_file,
+                 threshold=99):
+        self.normalization = normalization
+        self.bbi_file = bbi_file
+        self.bw = pyBigWig.open(bbi_file)
+        self.threshold = threshold
+        self.chrom_size = dict()
+
+        for name, size in self.bw.chroms().items():
+            if re.search('^(chr)?(\d+)?([XVI]+)?$', name):
+                self.chrom_size[name] = size
+
+        assert self.bw.isBigWig() or self.bw.isBigBed(),\
+        """The file passed should be either a bigWig or a bigBed"""
+        
+        if self.normalization == 'zscore':
+            self.mean = np.sum([size * self.bw.stats(name)[0]\
+                                for name, size in self.chrom_size.items()])
+            self.mean /= np.sum([size for name in self.chrom_size.values()])
+            self.std = np.sum([size * self.bw.stats(name, type='std')[0] for name,
+                               size in self.chrom_size.items()])
+            self.std /= np.sum([size for name in self.chrom_size.values()])
+
+        if self.normalization == 'max':
+            self.max = np.max([self.bw.stats(name, type='max')[0]\
+                               for name in self.chrom_size.keys()])
+
+        if self.normalization == 'perctrim':
+            sampled_values = get_sample(self.bw, self.chrom_size)
+            self.limit = np.percentile(sampled_values, self.threshold)
+            
+        if self.normalization == 'min_max':
+            self.max = np.max([self.bw.stats(name, type='max')[0]\
+                               for name in self.chrom_size.keys()])
+            self.min = np.min([self.bw.stats(name, type='min')[0]\
+                               for name in self.chrom_size.keys()])
+        self.bw.close()
+    
+    def __call__(self, seq):
+        if self.normalization == 'zscore':
+            return (seq - self.mean) / self.std
+        elif self.normalization == 'max':
+            return (seq / self.max)
+        elif self.normalization == 'perctrim':
+            seq[seq > self.limit] = self.limit
+            return seq
+        elif self.normalization is None:
+            return seq
+        elif self.normalization == 'logtransform':
+            return np.log(seq + 1)
+        elif self.normalization == 'min_max':
+            return (seq - self.min) / (self.max - self.min)
+        
+        
+class BiNormalizer(Normalizer):
+    
+    def __init__(self,
+                 normalization,
+                 bbi_file,
+                 threshold=99):
+        assert len(normalization) == 2,\
+        """BiNormalizer can only handle two successive normalization process"""
+        self.normalization = normalization[0]
+        self.threshold = threshold
+        self.first_normalizer = Normalizer(normalization[1],
+                                           bbi_file,
+                                           self.threshold)
+        self.bw = pyBigWig.open(bbi_file)
+        self.sample = self.first_normalizer(get_sample(self.bw,
+                                                       self.first_normalizer.chrom_size))
+        self.bw.close()
+        if self.normalization == 'zscore':
+            self.mean = np.mean(self.sample)
+            self.std = np.std(self.sample)
+
+        if self.normalization == 'max':
+            self.max = np.max(self.sample)
+
+        if self.normalization == 'perctrim':
+            self.limit = np.percentile(self.sample, self.threshold)
+            
+        if self.normalization == 'min_max':
+            self.max = np.max(self.sample)
+            self.min = np.min(self.sample)
+
+
+def get_sample(bw, chrom_size, sampling_len=1000):
+    """Returns a sample of the values of the bbi_file as a numpy array"""
+    sampled_values = []
+    for name, size in chrom_size.items():
+        sampling = np.random.randint(0, size - 1, sampling_len)
+        sampled_values.append([bw.values(name, sample, sample + 1)[0]\
+                               for sample in sampling])
+    
+    sampled_values =  np.array(sampled_values).reshape((len(sampled_values)*sampling_len,))
+    return sampled_values[np.isfinite(sampled_values)]
+
+
+class Weights(object):
+    """
+    info:
+        doc: >
+            Usefull to add weights to train a model. It calculate the probability
+            of every labels that a generator will generate and can leads
+            batches of weighting arrays corresponding to batches of labels.
+     args:
+        dataset:
+            the dataset of the generator.
+        weighting_mode:
+            {None, 'balanced', tuple(weights, bins), tuple(weight_pos, weight_neg)}
+            the methodology to set the weights.
+            For continuous dataset a tuple with the weights to apply and the
+            bins can be parsed (len(weights) == len(bins) - 1, and the smallest
+            bin must be smaller than the minimum of the data.)
+            For sparse dataset a tuple with the weight to apply for positive
+            (in [0]) and negative class (in [1]). Positive class refers to
+            labels with at least one positive value.
+            default: None
+        bins:
+            number of bins to apply to a continuous dataset before calculating
+            the probability of classes. Can also be an array of bins or 'auto'
+            for on optimized shearch of bins.
+            default='auto'
+    """
+    def __init__(self,
+                 dataset,
+                 weighting_mode=None,
+                 bins='auto'):
+        self.dataset = dataset
+        
+        self.command_dict = dataset.command_dict.get_details()
+
+        if 'sequence.SparseDataset' in self.command_dict:
+            try:
+                df = dataset.dataset.df
+            except AttributeError:
+                df = dataset.seq_dl.dataset.df
+            
+            if weighting_mode == 'balanced':
+                nb_neg = len(df[df.type ==  0])
+                nb_pos = len(df[df.type != 0])
+                
+                self.value_positive = (nb_neg + nb_pos) / float(nb_pos * 2)
+                self.value_negative = (nb_neg + nb_pos) / float(nb_neg * 2)
+
+            if isinstance(weighting_mode, tuple):
+                self.value_positive = weighting_mode[0]
+                self.value_negative = weighting_mode[1]
+
+        elif 'sequence.ContinuousDataset' in self.command_dict:
+            try:
+                chrom_size = dataset.dataset.chrom_size
+                norm_dico = dataset.dataset.extractor.norm_dico
+            except AttributeError:
+                chrom_size = dataset.seq_dl.dataset.chrom_size
+                norm_dico = dataset.seq_dl.dataset.extractor.norm_dico
+
+            samples = list()
+            annotation_files = self.command_dict['sequence.ContinuousDataset']['annotation_files']
+
+            if not isinstance(annotation_files, list):
+                annotation_files = [annotation_files]
+
+            for file in annotation_files:
+                bw = pyBigWig.open(file)
+                samples.append(norm_dico[file](get_sample(bw, chrom_size)))
+                bw.close()
+
+            if weighting_mode == 'balanced':
+                probas = list()
+                self.list_bins = list()
+
+                for sample in samples:
+                    proba, bins = self._get_proba(sample, bins)
+                    proba[proba == 0] = np.min(proba[proba > 0])
+                    probas.append(proba)
+                    self.list_bins.append(bins)
+
+                self.weights = [1 / proba for proba in probas]
+                self.weights = [weight / float(len(bins) - 1) for weight, bins\
+                                in zip(self.weights, self.list_bins)]
+
+            if isinstance(weighting_mode, tuple):
+                try:
+                    assert isinstance(weighting_mode[0], list)\
+                    and isinstance(weighting_mode[1], list),\
+                    """Weighting_mode should be 'balanced' or a tuple of list
+                    or a tuple of numpy arrays"""
+                except AssertionError:
+                    assert isinstance(weighting_mode[0], np.ndarray)\
+                    and isinstance(weighting_mode[1], np.ndarray),\
+                    """Weighting_mode should be 'balanced' or a tuple of list
+                    or a tuple of numpy arrays"""
+
+                assert len(weighting_mode[0]) == len(annotation_files)\
+                and len(weighting_mode[1]) == len(annotation_files),\
+                """Weights and bins must be parsed for every annotation_files"""
+
+                assert len(weighting_mode[0][0]) == len(weighting_mode[1][0]) + 1,\
+                """len(bins) must be equal to len(weights) + 1 !"""
+
+                self.weights = [weight for weight in weighting_mode[0]]
+                self.list_bins = [bins for bins in weighting_mode[1]]
+
+    def _get_proba(self, array, bins):
+        counts, values = np.histogram(array,
+                                      bins,
+                                      range=(min(array) - 0.001, max(array)),
+                                      density=True)
+        return counts * (values[1] - values[0]), values
+
+    def find_weights(self, seq):
+        if 'sequence.SparseDataset' in self.command_dict:
+            outputs = np.zeros((len(seq)))
+            outputs += self.value_negative
+            outputs[np.where(seq == 1)[0]] = self.value_positive
+            return outputs
+            
+        elif 'sequence.ContinuousDataset' in self.command_dict:
+            if len(seq.shape) == 4:
+                seq = seq.reshape((seq.shape[0],
+                                   seq.shape[1],
+                                   seq.shape[2] * seq.shape[3]))
+            elif len(seq.shape) == 2:
+                seq = seq.reshape(seq.shape + (1,))
+
+            outputs = np.zeros(seq.shape)
+
+            for i in range(seq.shape[2]):
+                digitized = np.digitize(seq[:, :, i],
+                                        self.list_bins[i],
+                                        right=True)
+                digitized[digitized >= len(self.list_bins[i])] = len(self.list_bins[i]) - 1
+                outputs[:, :, i] = self.weights[i][digitized - 1]
+            
+            return np.mean(outputs, axis=2)
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+            
