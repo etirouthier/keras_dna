7eec36046d39442c8a483d8090f61f9a7abc29f6
erouthier <etienne.routhier@upmc.fr>
2020-03-05 14:59:39 +0100

    Tensorflow 2 compatibility

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE/"README.md").read_text()
+
+setup(name="keras_dna",
+      version="0.0.1",
+      description="Build keras generator for genomic application",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url="https://github.com/etirouthier/keras_dna",
+      author="Etienne Routhier",
+      author_email="etienne.routhier@upmc.fr",
+      license="MIT",
+      classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6"],
+      packages=find_packages(),
+      include_packages_data=True,
+      install_requires=["keras",
+                        "tensorflow<1.15.0",
+                        "numpy",
+                        "pandas",
+                        "kipoi",
+                        "kipoiseq",
+                        "pyBigWig",
+                        "pybedtools"]
+      )
+      
