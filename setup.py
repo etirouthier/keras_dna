ac190b8b6825edbf65bb24b2e57fb3eda5b8c860
erouthier <etienne.routhier@upmc.fr>
2020-11-19 16:24:42 +0100

    Version 0.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE/"README.md").read_text()
 
 setup(name="keras_dna",
-      version="0.0.17",
+      version="0.0.18",
       description="Build keras generator for genomic application",
       long_description=README,
       long_description_content_type="text/markdown",
