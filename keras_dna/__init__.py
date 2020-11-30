04d122b5efc74556da478c86f08179c35720f641
erouthier <etienne.routhier@upmc.fr>
2020-11-30 15:07:31 +0100

    Version 0.0.20

--- keras_dna/__init__.py
+++ keras_dna/__init__.py
@@ -15,7 +15,7 @@ from .generators import Generator, MultiGenerator
 from .model import ModelWrapper
 from .sequence import SeqIntervalDl, StringSeqIntervalDl
 
-__version__ = '0.0.19'
+__version__ = '0.0.20'
 
 
 def get_ucsc():
