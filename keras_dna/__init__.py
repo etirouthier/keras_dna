8ee3b17894bd9474a926ba8e0bd2d0c6a9c7c44f
erouthier <etienne.routhier@upmc.fr>
2020-12-14 17:58:50 +0100

    Version 0.0.27

--- keras_dna/__init__.py
+++ keras_dna/__init__.py
@@ -15,7 +15,7 @@ from .generators import Generator, MultiGenerator
 from .model import ModelWrapper
 from .sequence import SeqIntervalDl, StringSeqIntervalDl
 
-__version__ = '0.0.26'
+__version__ = '0.0.27'
 
 
 def get_ucsc():
